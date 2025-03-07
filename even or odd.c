2de4b19c9287041826ce25a31521e6a77757fb40
Prathyusharaya <231fa04758@gmail.com>
2025-03-07 11:42:30 +0530

    Update even or odd.c

--- even or odd.c
+++ even or odd.c
@@ -3,14 +3,14 @@
 int main() {
     int number;
 
-    printf("Enter an integer: ");
+    printf("Enter an integer: "); \\ give integer
     scanf("%d", &number);
-
+ 
     if (number % 2 == 0) {
         printf("%d is even.\n", number);
     } else {
         printf("%d is odd.\n", number);
     }
 
-    return 0;
+    return 0;      
 }
